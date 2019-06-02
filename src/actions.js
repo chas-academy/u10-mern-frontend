1770d32a77cf5d2f9f311f7aeb33d101e30c390b
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-06-02 02:17:17 +0200

    Fix the 'SERVER' environment variable for development.

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const authenticate = () => dispatch => axios(`${process.env.SERVER}/api/user`, {
+const authenticate = () => dispatch => axios(`${process.env.REACT_APP_SERVER}/api/user`, {
   method: 'get',
   withCredentials: true,
 }).then((response) => {
@@ -15,7 +15,7 @@ const test = () => {
 };
 
 const getCourses = () => (dispatch) => {
-  axios(`${process.env.SERVER}/api/courses`, {
+  axios(`${process.env.REACT_APP_SERVER}/api/courses`, {
     method: 'get',
     withCredentials: true,
   }).then((response) => {
